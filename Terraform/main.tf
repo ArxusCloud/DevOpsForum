e14e4b3ee950fce6419aedc17d4f52e0dcfba1e1
Sujith Quintelier <sujith.quintelier@gmail.com>
2021-01-04 10:18:06 +0000

    Add Initial Terraform

---
+++ Terraform/main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-devopsforum-tf"
+    storage_account_name = "sadevopsforumtf"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "rg-devopsforum" {
+  name     = "rg-devopsforum"
+  location = "westeurope"
+}
